5a3c23b96a819f161d1a5165d4ddfaac5f96eb6f
Yi Yang <emeryyi@gmail.com>
2014-09-24 23:17:46 -0500

    1. beta release with glm model

---
+++ R/binomialfit.R
@@ -0,0 +1,10 @@
+binomialfit <- function(x, y) {
+	lassofit <- glmnet(x = x, y = y, family = "binomial", alpha = 1, maxit = 1e+6)
+    mcpfit <- ncvreg(X = x, y = y, family = "binomial", penalty = "MCP", warn = FALSE,
+        max.iter = 1e+6)
+    lasso.path <- as.matrix(lassofit$beta)
+    mcp.path <- as.matrix(mcpfit$beta[-1, ])
+    beta.path <- t(cbind(lasso.path, mcp.path))
+    candidate_models <- (1 - (beta.path == 0))
+	candidate_models
+}
