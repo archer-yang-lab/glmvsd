d13530afa085ac7b7efca5cb7123f6eef67ebffd
Yi Yang <emeryyi@gmail.com>
2015-03-20 01:58:49 -0500

    1. change maxit.iter from 1e6 to 1e4

--- R/binomialfit.R
+++ R/binomialfit.R
@@ -1,9 +1,9 @@
 binomialfit <- function(x, y) {
     lassofit <- glmnet(x = x, y = y, family = "binomial", alpha = 1, maxit = 1e+06)
     scadfit <- ncvreg(X = x, y = y, family = "binomial", penalty = "SCAD", 
-        warn = FALSE, max.iter = 1e+06)
+        warn = FALSE, max.iter = 1e+04)
     mcpfit <- ncvreg(X = x, y = y, family = "binomial", penalty = "MCP", 
-        warn = FALSE, max.iter = 1e+06)
+        warn = FALSE, max.iter = 1e+04)
     lasso.path <- as.matrix(lassofit$beta)
     scad.path <- as.matrix(scadfit$beta[-1, ])
     mcp.path <- as.matrix(mcpfit$beta[-1, ])
--- R/gaussianfit.R
+++ R/gaussianfit.R
@@ -1,9 +1,9 @@
 gaussianfit <- function(x, y) {
-    lassofit <- glmnet(x = x, y = y, alpha = 1, maxit = 1e+06)
+    lassofit <- glmnet(x = x, y = y, family = "gaussian", alpha = 1, maxit = 1e+06)
     scadfit <- ncvreg(X = x, y = y, family = "gaussian", penalty = "SCAD", 
-        max.iter = 1e+06)
+        warn = FALSE, max.iter = 1e+04)
     mcpfit <- ncvreg(X = x, y = y, family = "gaussian", penalty = "MCP", 
-        max.iter = 1e+06)
+        warn = FALSE, max.iter = 1e+04)
     lasso.path <- as.matrix(lassofit$beta)
     scad.path <- as.matrix(scadfit$beta[-1, ])
     mcp.path <- as.matrix(mcpfit$beta[-1, ])
