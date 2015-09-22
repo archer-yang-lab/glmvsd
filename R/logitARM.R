e5bf00cd62d7831922d8f334e3671f6a62bedd9f
Yi Yang <emeryyi@gmail.com>
2015-09-22 11:15:16 -0400

    1. logistf version of the code.

--- R/logitARM.R
+++ R/logitARM.R
@@ -1,47 +1,47 @@
 logitARM <- function(x, y, candidate_models, n_train, no_rep, psi, prior = TRUE) {
-  p <- NCOL(x)
-  n <- length(y)
-  n_mo <- NROW(candidate_models)
-  sk <- rowSums(candidate_models)
-  
-  wt_calc <- function(rep_id) {
-    wts <- rep(NA, n_mo)
-    tindex <- sample(n, n_train, replace = FALSE)
-    if (any(candidate_models[1, ] == 1)) {
-      for (j in seq(n_mo)) {
-        varindex <- (candidate_models[j, ] == 1)
-        glmfit <- brglm(y[tindex] ~ x[tindex, varindex], family = binomial)
-        if (any(is.na(glmfit$coef))) {
-          wts[j] <- 0
-        } else {
-          gk <- as.vector(cbind(1, x[-tindex, varindex]) %*% glmfit$coef)
-          fk <- ifelse(gk < 0, exp(gk)/(1 + exp(gk)), 1/(1 + exp(-gk)))
-          wts[j] <- prod(fk^(y[-tindex]) * (1 - fk)^(1 - y[-tindex]))
-        }  
-      }
-    } else {
-      wts[1] <- prod(mean(y[tindex])^(y[-tindex]) * (1 - mean(y[tindex]))^(1 - y[-tindex]))
-      for (j in seq(2, n_mo)) {
-        varindex <- (candidate_models[j, ] == 1)
-        glmfit <- brglm(y[tindex] ~ x[tindex, varindex], family = binomial)
-        if(any(is.na(glmfit$coef))) {
-          wts[j] <- 0
-        } else {
-          gk <- as.vector(cbind(1, x[-tindex, varindex]) %*% glmfit$coef)
-          fk <- ifelse(gk < 0, exp(gk)/(1 + exp(gk)), 1/(1 + exp(-gk)))
-          wts[j] <- prod(fk^(y[-tindex]) * (1 - fk)^(1 - y[-tindex]))
+    p <- NCOL(x)
+    n <- length(y)
+    n_mo <- NROW(candidate_models)
+    sk <- rowSums(candidate_models)
+    
+    wt_calc <- function(rep_id) {
+      wts <- rep(NA, n_mo)
+      tindex <- sample(n, n_train, replace = FALSE)
+      if (any(candidate_models[1, ] == 1)) {
+        for (j in seq(n_mo)) {
+          varindex <- (candidate_models[j, ] == 1)
+          glmfit <- logistf(y[tindex] ~ x[tindex, varindex])
+          if (any(is.na(glmfit$coef))) {
+            wts[j] <- 0
+          } else {
+            gk <- as.vector(cbind(1, x[-tindex, varindex]) %*% glmfit$coef)
+            fk <- ifelse(gk < 0, exp(gk)/(1 + exp(gk)), 1/(1 + exp(-gk)))
+            wts[j] <- prod(fk^(y[-tindex]) * (1 - fk)^(1 - y[-tindex]))
+          }  
+        }
+      } else {
+        wts[1] <- prod(mean(y[tindex])^(y[-tindex]) * (1 - mean(y[tindex]))^(1 - y[-tindex]))
+        for (j in seq(2, n_mo)) {
+          varindex <- (candidate_models[j, ] == 1)
+          glmfit <- logistf(y[tindex] ~ x[tindex, varindex])
+          if(any(is.na(glmfit$coef))) {
+            wts[j] <- 0
+          } else {
+            gk <- as.vector(cbind(1, x[-tindex, varindex]) %*% glmfit$coef)
+            fk <- ifelse(gk < 0, exp(gk)/(1 + exp(gk)), 1/(1 + exp(-gk)))
+            wts[j] <- prod(fk^(y[-tindex]) * (1 - fk)^(1 - y[-tindex]))
+          }
         }
       }
+      return(wts)
     }
-    return(wts)
-  }
-  library(parallel)
-  w_num <- matrix(unlist(mclapply(seq(no_rep), wt_calc)), nrow = no_rep, ncol = n_mo, byrow = TRUE)
-  
-  if (prior == TRUE) {
-    ck <- ck_compute(n_mo, sk, p)
-    w_num <- sweep(w_num, MARGIN = 2, exp(-psi * ck), "*")
-  }
-  weight <- colMeans(w_num/rowSums(w_num))
-  list(weight = weight)
+    library(parallel)
+    w_num <- matrix(unlist(mclapply(seq(no_rep), wt_calc)), nrow = no_rep, ncol = n_mo, byrow = TRUE)
+    
+    if (prior == TRUE) {
+        ck <- ck_compute(n_mo, sk, p)
+        w_num <- sweep(w_num, MARGIN = 2, exp(-psi * ck), "*")
+    }
+    weight <- colMeans(w_num/rowSums(w_num))
+    list(weight = weight)
 }
--- R/logitIC.R
+++ R/logitIC.R
@@ -1,28 +1,29 @@
 logitIC <- function(x, y, candidate_models, psi,
                     type = c("BIC", "AIC"), prior = TRUE) {
-  p <- NCOL(x)
-  n <- length(y)
-  type <- match.arg(type)
-  n_mo <- NROW(candidate_models)
-  sk <- rowSums(candidate_models)
-  ik <- rep(NA, n_mo)
-  if (any(candidate_models[1, ] == 1)) {
-    for (i in seq(n_mo)) {
-      glmfit <- brglm(y ~ x[, candidate_models[i, ] == 1], family = binomial)
-      ik[i] <- if (type == "BIC") extractAIC(glmfit, k=log(n))[2] else extractAIC(glmfit)[2]
-    }   
-  } else {
-    glmfit <- brglm(y ~ 1, family=binomial)
-    ik[1] <- if (type == "BIC") extractAIC(glmfit, k=log(n))[2] else extractAIC(glmfit)[2]
-    for (i in seq(2, n_mo)) {
-      glmfit <- brglm(y ~ x[, candidate_models[i, ] == 1], family = binomial)
-      ik[i] <- if (type == "BIC") extractAIC(glmfit, k=log(n))[2] else extractAIC(glmfit)[2]
+    p <- NCOL(x)
+    n <- length(y)
+	type <- match.arg(type)
+    n_mo <- NROW(candidate_models)
+    sk <- rowSums(candidate_models)
+    ik <- rep(NA, n_mo)
+    if (any(candidate_models[1, ] == 1)) {
+      for (i in seq(n_mo)) {
+        glmfit <- logistf(y ~ x[, candidate_models[i, ] == 1])
+        if (type == "BIC") ik[i] <- extractAIC(glmfit, k=log(n))[2]
+        else ik[i] <- extractAIC(glmfit)[2]
+      }   
+    } else {
+      ik[1] <- -2 * log(prod(mean(y)^y * (1 - mean(y))^(1 - y))) + sk[1] * log(n)
+      for (i in seq(2, n_mo)) {
+        glmfit <- logistf(y ~ x[, candidate_models[i, ] == 1])
+        if(type == "BIC") ik[i] <- extractAIC(glmfit, k=log(n))[2]
+        else ik[i] <- extractAIC(glmfit)[2]
+      }
     }
-  }
-  if (prior == TRUE) {
-    ck <- ck_compute(n_mo, sk, p)
-    ik <- ik + 2 * psi * ck
-  }
-  weight <- exp(-ik/2)/sum(exp(-ik/2))
-  list(weight = weight)
+    if (prior == TRUE) {
+      ck <- ck_compute(n_mo, sk, p)
+      ik <- ik + 2 * psi * ck
+    }
+    weight <- exp(-ik/2)/sum(exp(-ik/2))
+    list(weight = weight)
 }
