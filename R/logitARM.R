03e2d84c18fbc7a6d45030a3c5b7665bc8a31cc6
Yanjia Yu <yanjiayu1988@gmail.com>
2015-10-03 21:46:21 -0500

    1. bug fix for logitARM weight: log likelihood computing. 2. if in the intercept only case, the mean(y[tindex]) is 0 or 1, let the lw_num be -inf to prevent possible calculation error.

--- R/logitARM.R
+++ R/logitARM.R
@@ -15,11 +15,16 @@ logitARM <- function(x, y, candidate_models, n_train, no_rep, psi, prior = TRUE,
           lw_num[j] <- -Inf  
         } else {
           dk <- as.vector(cbind(1, x[-tindex, varindex]) %*% glmfit$coef)
-	      lw_num[j] <- sum((dk-log(1 + exp(dk))) * y[-tindex] - log(1+exp(dk)) * (1 - y[-tindex]))
+          fk <- ifelse(dk < 0, log(1 + exp(dk)), dk + log(1 + exp(-dk)))
+          lw_num[j] <- sum(y[-tindex] * dk) - sum(fk)
         }
       }
     } else {
-      lw_num[1] <- sum(log(mean(y[tindex])) * (y[-tindex]) + log(1 - mean(y[tindex])) * (1 - y[-tindex]))
+      if (mean(y[tindex])==0 | mean(y[tindex]==1)) {
+        lw_num[1] <- -Inf
+      } else {
+        lw_num[1] <- sum(log(mean(y[tindex])) * (y[-tindex]) + log(1 - mean(y[tindex])) * (1 - y[-tindex]))
+      }
       for (j in seq(2, n_mo)) {
         varindex <- (candidate_models[j, ] == 1)
         glmfit <- if(reduce_bias==TRUE) brglm(y[tindex] ~ x[tindex, varindex], family = binomial) else glm(y[tindex] ~ x[tindex, varindex], family = binomial) 
@@ -27,7 +32,8 @@ logitARM <- function(x, y, candidate_models, n_train, no_rep, psi, prior = TRUE,
           lw_num[j] <- -Inf  
         } else {
           dk <- as.vector(cbind(1, x[-tindex, varindex]) %*% glmfit$coef)
-	      lw_num[j] <- sum((dk-log(1 + exp(dk))) * y[-tindex] - log(1+exp(dk)) * (1 - y[-tindex]))
+          fk <- ifelse(dk < 0, log(1 + exp(dk)), dk + log(1 + exp(-dk)))
+          lw_num[j] <- sum(y[-tindex] * dk) - sum(fk)
         }
       }
     }
