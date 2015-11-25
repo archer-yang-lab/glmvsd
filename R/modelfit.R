05daaf5f4561227b1ebaa42229d9e7f791f0bfe9
Yi Yang <emeryyi@gmail.com>
2015-11-25 00:05:31 -0500

    1. correct some bugs

--- R/modelfit.R
+++ R/modelfit.R
@@ -1,20 +1,21 @@
 modelfit <- function(x, y, nfolds, 
-	penalty = c("lasso", "scad", "mcp"),
+	penalty = c("LASSO", "SCAD", "MCP"),
 	family=c("gaussian","binomial")) {
   penalty <- match.arg(penalty)
   family <- match.arg(family)
   y <- drop(y)
   y <- as.numeric(y)
   x <- as.matrix(x)
-  if(penalty == "lasso"){					
-    cvfit <- cv.glmnet(x=x,y=y,nfolds=nfolds,alpha=1,type.measure="mse",maxit=1e6,family=family)
-    coefit<-coef(cvfit,s="lambda.min")}
-  if(penalty == "mcp"){
-    cvfit<-cv.ncvreg(X=x,y=y,nfolds=nfolds,penalty="MCP",family=family,max.iter=1e4)
-    coefit<-cvfit$fit$beta[,cvfit$min]}
-  if(penalty == "scad"){
-    cvfit<-cv.ncvreg(X=x,y=y,nfolds=nfolds,penalty="SCAD",family=family,max.iter=1e4)
-    coefit<-cvfit$fit$beta[,cvfit$min]}
-  modelfit<- 1-(coefit[-1]==0)
+  if(penalty == "LASSO"){					
+    cvfit <- cv.glmnet(x=x,y=y,nfolds=nfolds,alpha=1,
+	    		type.measure="deviance",maxit=1e6,family=family)
+    coefit<-coef(cvfit,s="lambda.min")
+	}
+  if(penalty == "MCP" || penalty == "SCAD"){
+    cvfit<-cv.ncvreg(X=x,y=y,nfolds=nfolds,penalty=penalty,
+	    		family=family,max.iter=1e4)
+    coefit<-cvfit$fit$beta[,cvfit$min]
+	}
+  modelfit <- 1-(coefit[-1]==0)
   list(coefit = coefit, modelfit = modelfit)
 }
--- R/stability.test.R
+++ R/stability.test.R
@@ -1,5 +1,5 @@
 stability.test <- function(x, y, method=c("seq", "bs", "perturb"), 
-                           penalty = c("lasso", "scad", "mcp"), nrep = 50, 
+                           penalty = c("LASSO", "SCAD", "MCP"), nrep = 50, 
                            remove = 0.2, tau = 0.5, nfolds = 5, 
 					  family=c("gaussian","binomial")) {
   # model check
@@ -13,15 +13,15 @@ stability.test <- function(x, y, method=c("seq", "bs", "perturb"),
   n <- length(y)
   if (n != NROW(x)) 
     stop("x and y have different number of observations")
-  if (nrep < 10) 
-    stop("The number of repititions must be greater than 10.")
+  if (nrep < 2) 
+    stop("The number of repititions must be greater than 1.")
   if (remove<=0 || remove>=1)  
     stop("The proportion of data to be removed for sequential stability test must be in (0,1).")
   if (tau<=0 || tau>1) 
     stop("The perturbation size (tau) must be in (0,1].")
   # fit
   full <- modelfit(x, y, nfolds, penalty, family)
-  fitted <- drop(cbind(rep(1,n),x)%*%full$coefit)
+  fitted <- drop(as.matrix(cbind(rep(1,n),x)%*%full$coefit))
   sigmafit<-sqrt(sum((y-fitted)^2)/(n-sum(full$modelfit)))
   ####################sequential############
   model.sub<-matrix(NA, nrep, p)
@@ -33,9 +33,10 @@ stability.test <- function(x, y, method=c("seq", "bs", "perturb"),
     }
   }
   ####################Bootstrap####################
-  if(family=="gaussian" && method == "bs"){
+  if(method == "bs"){
     for (i in 1:nrep){
-      y.star <- rnorm(n,fitted,sigmafit)
+	    if(family=="gaussian") y.star <- rnorm(n,fitted,sigmafit)
+	    if(family=="binomial") y.star <- rbinom(n, 1, exp(fitted)/(1+exp(fitted)))
       model.sub[i, ] <- modelfit(x=x,y=y.star,nfolds,penalty)$modelfit
     }
   }
